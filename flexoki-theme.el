c467097097fa36dd40463a28c5675dfbc1f52ef0
Andrew Jose <arnav.jose@gmail.com>
2023-10-09 12:42:52 +0530

    package boilerplate

---
+++ flexoki-theme.el
@@ -0,0 +1,11 @@
+;;; flexoki-theme.el --- An inky color scheme for prose and code -*- lexical-binding:t -*-
+
+;; Copyright (C) 2022-2023  Free Software Foundation, Inc.
+
+;; Author: Andrew Jose <arnav.jose@gmail.com>
+;; Maintainer: Andrew Jose <arnav.jose@gmail.com>
+;; URL: https://github.com/crmsnbleyd/flexoki-emacs
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: faces, theme, accessibility
+
+;; This file is NOT part of GNU Emacs.
