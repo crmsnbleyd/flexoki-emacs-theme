4285faa2f6fd95c46b42fa922e3309f6a0ad0711
Andrew Jose <arnav.jose@gmail.com>
2023-10-17 10:31:38 +0530

    remove newline from commentary

--- flexoki-themes.el
+++ flexoki-themes.el
@@ -13,7 +13,6 @@
 ;; This file is NOT part of GNU Emacs.
 
 ;;; Commentary:
-;;
 ;; The `flexoki-themes' is a pair of light and dark themes for GNU
 ;; Emacs based on the Flexoki colour scheme by Steph Ango.
 
