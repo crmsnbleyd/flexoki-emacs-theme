f0a6b21f7ecdd0852ecfaf89149025c176a414ef
Drew Jose <mail@drewsh.com>
2025-03-11 05:55:28 +0530

    remove ci

--- .github/workflows/init.el
+++
@@ -1,6 +0,0 @@
-(progn
-  (require 'package)
-  (push '("melpa" . "https://melpa.org/packages/") package-archives)
-  (package-initialize)
-  (unless (seq-find (lambda (e) (string= "melpa" (package-desc-archive (cadr e)))) package-archive-contents)
-    (package-refresh-contents)))
